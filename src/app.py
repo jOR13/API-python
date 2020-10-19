8ac779bc4a3718989adffae294168be1b07c9663
jor13 <ochoar13@hotmail.com>
2020-10-19 12:35:28 -0600

    .

--- src/app.py
+++ src/app.py
@@ -68,4 +68,6 @@ def updateUser(id):
     return jsonify({'msg': 'user updated'})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
